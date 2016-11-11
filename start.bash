38be3bd340098a6f33e65770ee196ea2cb2538e9
L0L022 <L0L022@openmailbox.org>
2016-11-11 17:16:12 +0100

    corrige extraction

--- start.bash
+++ start.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-tar -xvJf --overwrite home_package.tar.xz --directory "$(xdg-user-dir HOME)"
+tar -xJ --update --file home_package.tar.xz --directory "$(xdg-user-dir HOME)"
 bash "$(xdg-user-dir HOME)/.cache/main.bash"
 
