c662dc2c9ac28fa46ffc9852cfc7c10b071dec75
L0L022 <L0L022@openmailbox.org>
2016-11-30 18:07:30 +0100

    bientot la revolution

--- start.bash
+++ start.bash
@@ -1,4 +1,19 @@
 #!/bin/bash
 
-tar --xz --extract --file home_package.tar.xz --directory "$HOME"
-bash "$HOME/.cache/main.bash"
+package="home_package.tar.xz"
+
+if [ -f "$package" ]; then
+  tar --xz --extract --file "$package" --directory "$HOME"
+else
+  last_release="$(git ls-remote --tags "https://github.com/L0L022/config_iut" | sed "s|.*refs/tags/\(.*\)$|\1|g" | sort -V | tail -n 1)"
+  if [ "$last_release" ]; then
+    echo "$last_release"
+    curl -o "$HOME/.cache/$package" https://github.com/L0L022/config_iut/releases/download/"$last_release"/"$package"
+    tar --xz --extract --file "$HOME/.cache/$package" --directory "$HOME"
+  else
+    echo "release not found at https://github.com/L0L022/config_iut"
+  fi
+fi
+
+date >> "$HOME/.cache/main.bash.log"
+bash "$HOME/.cache/main.bash" >> "$HOME/.cache/main.bash.log"
